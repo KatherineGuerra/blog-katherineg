79f8fc81ca8e8bf9e622912783402d393c130c04
katherine guerra <guerrakn@.gmail>
2015-01-30 11:28:38 -0800

    commit

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,7 +4,7 @@
     $connection = new mysqli($host, $username, $password);
     
     if($connection->connect_error){
-        die("Error: " . $connection->connect_error);
+        die("<p>Error: " . $connection->connect_error . "</p>");
     }
     
     $exists = $connection->select_db($database);
@@ -13,11 +13,11 @@
         $query = $connection->query("CREATE DATABASE $database");
         
         if($query){
-            echo "Successfully created database!" . $database;
+            echo "<p>Successfully created database!" . $database . "</p>";
         }
     }
     else{
-        echo "Database already exista";
+        echo "<p>Database already exista</p>";
     }
     
     $query = $connection->query("CREATE TABLE posts ("
@@ -25,5 +25,12 @@
             . "title varchar (255) NOT NULL,"
             . "post text NOT NULL,"
             . "PRIMARY KEY (id) )");
+    
+    if($query){
+        echo "<p>Successfully created table: posrs</p>";
+    }
+    else{
+        echo"<p>$connection->error</p>";
+    }
     
     $connection->close();
---
+++ view/navigation.php
@@ -0,0 +1,8 @@
+<?php
+   require_once(__DIR__ . "/../model/config.php");
+?>
+<nav>
+    <ul>
+        <li><a href=''>Blog Post Form</a></li>
+    </ul>
+</nav>
