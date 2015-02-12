534b11201d0794a1c20ff52401eede8c4c64fd76
katherine guerra <guerrakn@.gmail>
2015-02-12 11:28:45 -0800

    2-12-15

---
+++ controller/read-posts.php
@@ -0,0 +1,12 @@
+<?php
+   require_once(__DIR__ . "/../model/config.php");
+   
+   $query = "SELECT * FROM posts";
+   $result = $_SESSION["connection"]->query($query);
+   
+   if($result){
+       while($row = mysqli_fetch_array($result)){
+           echo "<div class='post'>";
+           echo "";
+       }
+   }
